eb0490015aedb4919190f0f145f3c3392f277588
Mihai <kazepworld@gmail.com>
2019-03-19 21:40:21 +0100

    Add entity models

---
+++ 52North.Model/Models/ProcessSummary.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace _52North.Model.Models
+{
+    [Serializable]
+    [XmlRoot(ElementName = "ProcessSummary", Namespace = WpsNamespace)]
+    public class ProcessSummary
+    {
+
+        private const string OwsNamespace = "http://www.opengis.net/ows/2.0";
+        private const string WpsNamespace = "http://www.opengis.net/wps/2.0";
+
+        [XmlElement("Title", Namespace = OwsNamespace)]
+        public string Title { get; set; }
+
+        [XmlElement("Identifier", Namespace = OwsNamespace)]
+        public string Identifier { get; set; }
+
+        [XmlAttribute("processVersion", Namespace = WpsNamespace)]
+        public string Version { get; set; }
+
+    }
+}
---
+++ 52North.Model/Models/ProcessSummaryCollection.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace _52North.Model.Models
+{
+    [Serializable]
+    [XmlRoot(ElementName = "Capabilities", Namespace = WpsNamespace)]
+    public class ProcessSummaryCollection
+    {
+
+        private const string WpsNamespace = "http://www.opengis.net/wps/2.0";
+
+        [XmlArray("Contents")]
+        [XmlArrayItem(ElementName = "ProcessSummary", Type = typeof(ProcessSummary), Namespace = WpsNamespace)]
+        public ProcessSummary[] Processes { get; set; }
+
+    }
+}
