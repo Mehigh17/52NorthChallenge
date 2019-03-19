c6d040a2e33e2670761ff889ae72d2b846af8f6e
Mihai <kazepworld@gmail.com>
2019-03-20 00:38:12 +0100

    Add delegate command

---
+++ 52North.Model/Commands/DelegateCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace _52North.Model.Commands
+{
+    public class DelegateCommand : ICommand
+    {
+
+        private readonly Action _action;
+        private readonly Func<bool> _canExecute;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        public DelegateCommand(Action action, Func<bool> canExecute)
+            : this(action)
+        {
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+    }
+}
