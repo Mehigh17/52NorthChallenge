b72f4cdcaff12418f98b7c57d7948178f6e9db4f
Mihai <kazepworld@gmail.com>
2019-04-03 20:45:12 +0200

    Add the ArcGIS Pro add-in project set up with IoC

---
+++ src/52North.ArcGis/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using _52North.Model.ViewModels;
+
+namespace _52North.ArcGis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(MainWindowViewModel viewModel) : this()
+        {
+            DataContext = viewModel;
+        }
+
+    }
+}
---
+++ src/52North.ArcGis/ShowMainWindow.cs
@@ -0,0 +1,54 @@
+﻿using _52North.Model.Services;
+using _52North.Model.ViewModels;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using DryIoc;
+
+namespace _52North.ArcGis
+{
+    internal class ShowMainWindow : Button
+    {
+
+        private MainWindow _mainwindow = null;
+        private readonly IContainer _container;
+
+        public ShowMainWindow()
+        {
+            _container = CreateContainer();
+        }
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_mainwindow != null)
+                return;
+
+            _mainwindow = _container.Resolve<MainWindow>();
+            _mainwindow.Owner = FrameworkApplication.Current.MainWindow;
+            _mainwindow.Closed += (o, e) => { _mainwindow = null; };
+            _mainwindow.Show();
+            //uncomment for modal
+            //_mainwindow.ShowDialog();
+        }
+
+        /// <summary>
+        /// Creates an IoC container and registers the needed services.
+        /// </summary>
+        private static IContainer CreateContainer()
+        {
+            var container = new Container();
+
+            // Services
+            container.Register<IWpsClient, WpsClient>(Reuse.Singleton);
+
+            // View Models
+            container.Register<MainWindowViewModel>();
+
+            // Views
+            container.Register(made: Made.Of(() => new MainWindow(Arg.Of<MainWindowViewModel>())));
+
+            return container;
+        }
+
+    }
+}
